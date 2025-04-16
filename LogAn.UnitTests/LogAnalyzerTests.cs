4e0abd2c839f49a44f89d48e570e74386f219d7c
German <german@german.com>
2025-04-16 21:45:31 +0300

    Добавлен пример применения категорий тестов

--- LogAn.UnitTests/LogAnalyzerTests.cs
+++ LogAn.UnitTests/LogAnalyzerTests.cs
@@ -12,8 +12,15 @@ namespace LogAn.UnitTests
             return new LogAnalyzer();
         }
 
+        [Test]
+        [Ignore("В этом тесте имеется ошибка")]
+        public void IsValidFileName_ValideFile_ReturnsTrue()
+        {
+            //...
+        }
 
         [Test]
+        [Category("Fast tests")]
         public void IsValidFileName_EmptyFileName_Throws()
         {
             LogAnalyzer la = MakeAnalyzer();
@@ -22,20 +29,21 @@ namespace LogAn.UnitTests
             ex.Message);
         }
 
-            [Test]
+        [Test]
         public void IsValidLogFileName_BadExtension_ReturnsFalse()
         {
             LogAnalyzer analyzer = new LogAnalyzer();
             bool result = analyzer.IsValidLogFileName("filelewithbadextension.foo");
-            Assert.That(result,Is.False);
+            Assert.That(result, Is.False);
         }
 
         [Test]
+        [Category("Fast tests")]
         public void IsValidLogFileName_GoodExtensionLowercase_ReturnsTrue()
         {
             LogAnalyzer analyzer = new LogAnalyzer();
             bool result = analyzer.IsValidLogFileName("filewithgoodextension.slf");
-            Assert.That(result,Is.True);
+            Assert.That(result, Is.True);
         }
         [Test]
         public void IsValidLogFileName_GoodExtensionUppercase_ReturnsTrue()
