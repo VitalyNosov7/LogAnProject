4fb00c8606602cc126f2dc639ddc092451291a69
German <german@german.com>
2025-04-19 19:24:42 +0300

    Тесты класса калькулятор, показывающие работу проверки состояния и изменения состояния объекта

---
+++ LogAn.UnitTests/MemCalculatorTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    public class MemCalculatorTests
+    {
+        private static MemCalculator MakeCalc()
+        {
+            return new MemCalculator();
+        }
+
+        [Test]
+        public void Sum_ByDefault_ReturnsZero()
+        {
+            MemCalculator calc = MakeCalc();
+            int lastSum = calc.Sum();
+            //            Assert.AreEqual(0, lastSum);
+            Assert.That(lastSum, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Add_WhenCalled_ChangesSum()
+        {
+            MemCalculator calc = MakeCalc();
+            calc.Add(1);
+            int sum = calc.Sum();
+            // Assert.AreEqual(1, sum);
+            Assert.That(sum, Is.EqualTo(1));
+        }
+     }
+}
